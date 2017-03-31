ef713642aa29bc42e04b39278d242a447f47e58a
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2017-03-31 07:24:46 -0500

    first commit

---
+++ history.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: History Developer
+Plugin URI: http://cesaraquino.com.pe
+Description: Have a history of what you are building in the project.
+Version: 0.1
+Author: César Aquino Maximiliano
+Plugin URI: http://cesaraquino.com.pe
+*/
+?>
