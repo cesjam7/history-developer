60dee3f2150c6ed4ed8c80dd1d55b3a775a6c25b
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2017-03-31 07:51:00 -0500

    agregando estilos

--- history-setting.php
+++ history-setting.php
@@ -14,9 +14,9 @@ function c7hd_settings_page() { ?>
 
         <form method="post" action="options.php">
 
-            <div class="o612-form-group">
+            <div class="c7hd-form-group">
                 <label for="c7hd" class="c7hd-label">Input your note:</label>
-                <textarea name="nota" class="c7hd-input"></textarea>
+                <textarea name="nota" class="c7hd-input" rows="6"></textarea>
             </div>
 
             <p class="submit">
@@ -24,6 +24,20 @@ function c7hd_settings_page() { ?>
             </p>
 
         </form>
+
+        <h2>Past Notes</h2>
+        <div class="c7hd-note">
+            <p><strong>07-05-2016 18:02</strong></p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="c7hd-note">
+            <p><strong>07-05-2016 18:02</strong></p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="c7hd-note">
+            <p><strong>07-05-2016 18:02</strong></p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
     </div>
     <?php
 }
--- history.php
+++ history.php
@@ -12,6 +12,12 @@ Plugin URI: http://cesaraquino.com.pe
 define('C7HD_ROOTDIR', plugin_dir_path(__FILE__));
 define('C7HD_URLPLUGIN', plugin_dir_url( __FILE__ ));
 
+// Agregar css para los formularios en el administrador
+function c7hd_admin_assets() {
+    wp_enqueue_style( 'c7hd_admin_css', C7HD_URLPLUGIN . 'assets/css/main.css', false, '1.0' );
+}
+add_action( 'admin_enqueue_scripts', 'c7hd_admin_assets' );
+
 // Llamando al archivo del panel de configuración
 require_once(C7HD_ROOTDIR . 'history-setting.php');
 ?>
